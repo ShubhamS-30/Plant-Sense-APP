585ba7b2611886133d2deffbdd61160ce2b263d2
SHUBHAM_SAHU <shubham30sahu101@gmail.com>
2023-07-26 14:16:58 +0530

    native features

--- lib/disease_classification.dart
+++ lib/disease_classification.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:plant_sense_1/image_input.dart';
+import 'package:plant_sense_1/predictions.dart';
 class DiseaseClassification extends StatefulWidget {
   const DiseaseClassification({super.key});
 
@@ -16,8 +17,25 @@ class _DiseaseClassificationState extends State<DiseaseClassification> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            SizedBox(height: 100,),
-            Text("CLASSIFICATION"),
+            SizedBox(
+              height: 80,
+            ),
+            Text(
+              "DISEASE CLASSIFICATION",
+              style: TextStyle(color: Colors.green, fontSize: 30),
+            ),
+            // const SizedBox(
+            //   height: 200,
+            // ),
+            // FilledButton(
+            //   onPressed: () {},
+            //   style: FilledButton.styleFrom(backgroundColor: Colors.green),
+            //   child: const Text('Take a Picture',style: TextStyle(fontSize: 25),),
+            // ),
+            SizedBox(height: 30,),
+            ImageInput(),
+            SizedBox(height: 40,),
+            GetPredictions(),
           ],
         ),
       ),
